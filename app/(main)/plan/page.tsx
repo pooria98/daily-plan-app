a431d86fc237593ae58fec099803263032a53424
pooria98 <pooriakheyri@yahoo.com>
2024-12-02 20:37:43 +0330

    🚧 :poop: plan page

--- app/(main)/plan/page.tsx
+++ app/(main)/plan/page.tsx
@@ -1,65 +1,57 @@
-import React from "react";
+"use client";
+import { TextInput, NumberInput, Button, ActionIcon, Group, Divider } from "@mantine/core";
+import { randomId } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { FaTrash } from "react-icons/fa6";
 
-export default function page() {
+export default function PlanPage() {
+  const form = useForm({
+    mode: "controlled",
+    initialValues: {
+      activities: [{ title: "", hour: 0, key: randomId() }],
+    },
+  });
+
+  const fields = form.getValues().activities.map((item, index) => (
+    <div key={item.key} className="flex flex-wrap items-center gap-2 mb-2">
+      <TextInput
+        placeholder="Title"
+        className="flex-[1_1_250px]"
+        key={form.key(`activities.${index}.title`)}
+        {...form.getInputProps(`activities.${index}.title`)}
+      />
+      <NumberInput
+        hideControls
+        className="flex-[0_1_60px]"
+        key={form.key(`activities.${index}.hour`)}
+        {...form.getInputProps(`activities.${index}.hour`)}
+        suffix="h"
+      />
+      <ActionIcon color="red" size="lg" onClick={() => form.removeListItem("activities", index)}>
+        <FaTrash size="1rem" />
+      </ActionIcon>
+    </div>
+  ));
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus odit nemo recusandae a
-      aperiam, quam dolores est maiores earum modi eius commodi itaque laudantium deserunt
-      laboriosam voluptate quas minus vitae inventore fuga ut suscipit repellendus soluta sunt? Ab
-      nesciunt libero deserunt, voluptas adipisci totam perferendis magni enim dolorum consequuntur
-      corporis ea excepturi explicabo distinctio numquam qui quibusdam officiis aperiam vero,
-      architecto, assumenda ex tenetur dolores. Excepturi nisi earum dignissimos quidem nihil minus
-      ipsum! Voluptatibus saepe quasi inventore, eum porro veritatis placeat odit dolore impedit
-      facilis nam consequatur accusantium voluptates? Pariatur voluptatem, ex est excepturi a quidem
-      commodi sequi dicta quam nobis veritatis quis ducimus quibusdam eveniet quae voluptatum porro
-      beatae hic atque! Magni at eligendi nostrum sed? Eligendi ipsa numquam ex obcaecati id, magnam
-      velit possimus ea ipsum veniam recusandae iste sapiente. Quisquam voluptas magni iure eaque
-      laudantium quidem maiores corrupti voluptate enim, libero animi fugiat et dolore totam
-      molestiae placeat, architecto ullam perferendis voluptatibus minima illum dolorem at
-      aspernatur qui. Dolor deserunt qui ullam iste eaque! Quis recusandae necessitatibus optio modi
-      perspiciatis qui sapiente. Aut sapiente beatae tempora veritatis voluptates labore sed
-      repudiandae aperiam? Animi corrupti veritatis, praesentium dolor velit sequi quis vel aut
-      excepturi, quaerat magni qui! Rem a dolores ea placeat porro, aliquid deserunt quis eius
-      doloribus illo cum tenetur odio corporis commodi! Voluptates ratione vel eveniet? Hic, cumque
-      sint quaerat dolore adipisci provident laboriosam doloribus animi eius! Quasi dolor similique
-      quos unde facere, laudantium at minus, esse fugit ipsum inventore aliquam! Id itaque vel quae
-      ab ipsa blanditiis, laborum necessitatibus magnam perspiciatis voluptatem assumenda sed minima
-      recusandae tenetur explicabo veniam nihil tempora quos est optio quas cum. Quod animi fugit
-      commodi facilis delectus incidunt deserunt, quos soluta, atque ipsum eos ad illum. Odio
-      dolore, omnis amet, obcaecati inventore enim voluptates ipsum sed eius quas dolores at
-      adipisci ipsam accusamus temporibus fugit sapiente atque nemo labore nesciunt culpa incidunt?
-      Necessitatibus dolore eveniet debitis quod iusto earum sed numquam minus maiores, totam
-      explicabo distinctio et. Adipisci repudiandae libero placeat sint sunt. Quasi incidunt natus
-      cum perferendis, impedit ullam dignissimos, assumenda vero asperiores, voluptas eius alias
-      non? Nemo delectus cum veritatis ea officia autem dicta iusto iste, itaque asperiores
-      necessitatibus blanditiis! Praesentium facilis animi magni a. Et beatae corporis
-      necessitatibus molestias velit repellendus praesentium natus officiis sequi quia debitis,
-      dolores itaque ipsam! Mollitia expedita facere iusto recusandae illum earum in vitae
-      voluptates unde saepe. Quod, quasi. Itaque accusamus porro iure quam! Corrupti repellat maxime
-      nihil magnam fuga consequuntur assumenda voluptatum voluptatibus neque in cum dolor delectus
-      expedita earum dolores veritatis libero dolorem, repudiandae ad atque! Hic exercitationem
-      tempore labore, perferendis esse id in, est, enim dignissimos minima nam corporis sapiente
-      harum voluptate reprehenderit? Ipsa nulla labore a. Magnam placeat aliquam ipsam iste et
-      voluptatibus, odio sed voluptates assumenda facilis cupiditate quae libero tenetur quaerat,
-      atque harum omnis consectetur! Voluptatem nemo nostrum debitis aperiam sed. Aperiam maxime ad
-      animi eligendi, sit autem dolores enim temporibus consectetur vero, omnis distinctio
-      cupiditate cumque perspiciatis! Ipsum error iste a, dolore itaque mollitia excepturi adipisci
-      enim maiores consectetur quod consequatur dolor tempore at? Sapiente perspiciatis excepturi
-      ullam doloremque, iure obcaecati voluptatibus eum officiis deserunt earum totam dolore
-      similique nulla aspernatur ex. Eaque ipsum repellendus asperiores adipisci consectetur. Sequi
-      voluptatum aliquam et voluptates cupiditate nihil ad nulla qui at in fuga recusandae possimus,
-      ratione cum explicabo quisquam repudiandae voluptas earum. Nobis ipsam hic laudantium vel eum
-      dolorem? At fugit iusto, harum nulla similique, deserunt beatae autem exercitationem eveniet
-      assumenda rem nesciunt praesentium. Est dolorem consequatur veritatis saepe laudantium nihil
-      officia asperiores ipsam, minus fugit magni repudiandae repellat nisi corrupti, vero assumenda
-      ipsa nulla culpa. Ipsam nemo fugit voluptate quaerat amet a suscipit perspiciatis commodi,
-      natus, atque ratione debitis magni totam ut et excepturi vero deserunt tenetur obcaecati
-      itaque eveniet. Similique, nostrum. Repellendus, neque a cupiditate sed incidunt impedit.
-      Labore cupiditate voluptate facilis quibusdam nisi, illum necessitatibus? Repellendus
-      doloremque veniam soluta amet placeat. Vel adipisci sapiente a ipsam minima facilis quia sed
-      vero possimus officiis suscipit quod voluptatibus expedita quisquam molestias, dolorum natus,
-      laborum fugit aspernatur id culpa sint quae. Similique quis ex officiis error dolorem!
-      Aspernatur deleniti, repellendus aliquid cupiditate facere veritatis! Rem reiciendis
+    <div className="w-full max-w-[600px] min-h-[600px] mx-auto flex justify-center items-center">
+      <div>
+        {fields}
+        <Group justify="space-between" mt="md">
+          <Button
+            onClick={() =>
+              form.insertListItem("activities", { title: "", hour: 0, key: randomId() })
+            }
+          >
+            Add activity
+          </Button>
+          <p>SUM: {form.getValues().activities.reduce((a, b) => a + b.hour, 0)}</p>
+        </Group>
+        <Divider my="xs" />
+        <Button type="submit" fullWidth>
+          Submit
+        </Button>
+      </div>
     </div>
   );
 }
