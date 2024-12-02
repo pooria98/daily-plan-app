17dd358ab5a82a3af3d46dbe9e6a8c7b70126f92
pooria98 <pooriakheyri@yahoo.com>
2024-12-02 14:45:10 +0330

    prevent signed-in users to see signin and signup pages

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,6 +1,17 @@
 import Signin from "@/components/signin/Signin";
+import { auth } from "@/auth"; // path to your Better Auth server instance
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="auth-container">
       <Signin />
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,17 @@
 import Signup from "@/components/signup/Signup";
+import { auth } from "@/auth"; // path to your Better Auth server instance
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function SignupPage() {
+export default async function SignupPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="auth-container">
       <Signup />
