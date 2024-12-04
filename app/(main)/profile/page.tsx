08be9a09028bdc19a2ff31d04022b84d2ba30058
pooria98 <pooriakheyri@yahoo.com>
2024-12-04 17:37:03 +0330

    🚧 add profile page

---
+++ app/(main)/profile/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ProfilePage() {
+  return (
+    <div className="w-full min-h-[600px] flex flex-col justify-center items-center">
+      <p className="text-2xl font-bold">Profile Page</p>
+      <p>WIP</p>
+    </div>
+  );
+}
